2d16269718ec763ce9f0362ce05e29e7bf3173e4
nousi <nousidayo@gmail.com>
2020-11-22 14:57:17 +0900

    Add form of register in login.vue

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -29,7 +29,43 @@
                 </form>
             </div>
         </div>
-        <div class="panel" v-show="tab === 2">Register Form</div>
+        <div class="panel" v-show="tab === 2">
+            <form class="form" @submit.prevent="register">
+                <label for="username">Name</label>
+                <input
+                    type="text"
+                    class="form__item"
+                    id="username"
+                    v-model="registerForm.name"
+                />
+                <label for="email">Email</label>
+                <input
+                    type="text"
+                    class="form__item"
+                    id="email"
+                    v-model="registerForm.email"
+                />
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    class="form__item"
+                    id="password"
+                    v-model="registerForm.password"
+                />
+                <label for="password-confirmation">Password (confirm)</label>
+                <input
+                    type="password"
+                    class="form__item"
+                    id="password-confirmation"
+                    v-model="registerForm.password_confirmation"
+                />
+                <div class="form__button">
+                    <button type="submit" class="button button--inverse">
+                        register
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
