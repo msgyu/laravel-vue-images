aaf9d6c8efa7e9ccba58382bddaca365db00b862
nousi <nousidayo@gmail.com>
2020-11-22 16:34:37 +0900

    Add store in index.js

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,3 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 import "es6-promise/auto";
 import auth from "./auth";
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    modules: {
+        auth
+    }
+});
+
+export default store;
