9c4eef5ce4c88282deba86c5d69d7ae85a5c7533
nousi <nousidayo@gmail.com>
2020-11-22 14:09:20 +0900

    Add loginform in Login.vue

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -4,7 +4,25 @@
             <li class="tab__item" @click="tab = 1">Login</li>
             <li class="tab__item" @click="tab = 2">Register</li>
         </ul>
-        <div class="panel" v-show="tab === 1">Login Form</div>
+        <div class="panel" v-show="tab === 1">
+            <div class="panel" v-show="tab === 1">
+                <form class="form">
+                    <label for="login-email">Email</label>
+                    <input type="text" class="form__item" id="login-email" />
+                    <label for="login-password">Password</label>
+                    <input
+                        type="password"
+                        class="form__item"
+                        id="login-password"
+                    />
+                    <div class="form__button">
+                        <button type="submit" class="button button--inverse">
+                            login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
         <div class="panel" v-show="tab === 2">Register Form</div>
     </div>
 </template>
